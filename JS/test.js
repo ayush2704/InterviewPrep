27b779b49467f48e372bb9cbff1c219b3377b591
ayush2704 <ayush.ak471999@gmail.com>
2026-03-22 05:35:38 +0530

    first commit

---
+++ JS/test.js
@@ -0,0 +1,10 @@
+// millions of user
+//
+// 2 transactions modifying same account a1&a2 
+// transfer from a1 to a2 and transfer a2 to a1
+
+/// ex- isolation  serialise the transaction 
+// 100 tb of s3 data 
+// parent folder->folder->//recording. whichever are expired needs to be deleted 
+                          //reports
+                          // parentfolder/folder/recording/*    TTL 
